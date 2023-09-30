9ef415c39243a1b2c4cdb466092d22a544f6cd30
vaishnavi <landevaishnavi342@gmail.com>
2023-09-30 18:41:11 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+  Strings: ["Frontend Development", "Graphic Designer", "Content Writer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
